27b57513d7dc745c989d4422e536fc1c923e2ad5
Alexandr-ai <bodnaro926@gmail.com>
2025-03-07 01:13:24 +0200

    fix bug: re_password v2.0

--- UA_13XX_bravo/settings.py
+++ UA_13XX_bravo/settings.py
@@ -58,8 +58,8 @@ DJOSER = {
     "ACTIVATION_URL": "auth/activate/{uid}/{token}/",
     "PASSWORD_RESET_CONFIRM_URL": "auth/password-reset-confirm/{uid}/{token}/",
     "SERIALIZERS": {
-        "user_create": "your_app.serializers.UserCreateSerializer",  
-        "user": "your_app.serializers.UserSerializer",  
+        "user_create": "users.serializers.UserCreateSerializer",  
+        "user": "users.serializers.UserSerializer",  
     },
 }
 
