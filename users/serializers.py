fa27731d3cec6c343ee69b10389790a80a2c40f4
Alexandr-ai <bodnaro926@gmail.com>
2025-03-06 16:57:40 +0200

    change: update м2.0 funtion in users/serializer.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,23 +1,6 @@
 from rest_framework import serializers
 from users.models import User
 
-# class UserSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True, min_length=8, required=True)
-
-#     class Meta:
-#         model = User
-#         fields = ['id', 'email', 'password', 'first_name', 'last_name', 'phone']
-
-#     def create(self, validated_data):
-#         user = User(
-#             email=validated_data["email"],
-#             first_name=validated_data.get("first_name", ""),
-#             last_name=validated_data.get("last_name", ""),
-#             phone=validated_data.get("phone", "")
-#         )
-#         user.set_password(validated_data["password"])
-#         user.save()
-#         return user
     
 
 
