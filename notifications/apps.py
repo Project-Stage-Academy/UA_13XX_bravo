98098f1845e2cde502a03ec3cbe82f4481f9e43e
Alexandr-ai <bodnaro926@gmail.com>
2025-03-22 18:28:10 +0200

    change: review changes v1.0

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
 
-class InvestmentsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "investments"
-
-
 class NotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "notifications"
--- notifications/signals.py
+++ notifications/signals.py
@@ -2,28 +2,29 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Notification, Type
 from companies.models import CompanyFollowers
-from users.models import User
+from django.contrib.auth import get_user_model
 from .utils import send_email_notification
+import logging
+
+logger = logging.getLogger(__name__)
+User = get_user_model()
 
 @receiver(post_save, sender=CompanyFollowers)
 def create_notification_for_new_follower(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(
-            user=instance.investor,  
-            type=Type.objects.get(name="new_follower"),
-            entity=instance.startup,
-            content=f"{instance.investor.username} started following {instance.startup.company_name}"
-        )
-        send_email_notification(instance.investor, "new_follower", f"{instance.investor.username} started following {instance.startup.company_name}")
+    if not created:
+        return
 
-# це на випалок якщо потрібно буде меседжі надсилати по чаті використовуючи communications
-# @receiver(post_save, sender=Message)
-# def create_notification_for_new_message(sender, instance, created, **kwargs):
-#     if created:
-#         Notification.objects.create(
-#             user=instance.recipient,
-#             type=Type.objects.get(name="new_message"),
-#             entity=instance,
-#             content=f"New message from {instance.sender.username}"
-#         )
-#         send_email_notification(instance.recipient, "new_message", f"New message from {instance.sender.username}")
+    try:
+        notif_type, _ = Type.objects.get_or_create(name="new_follower")
+        Notification.objects.create(
+            user=instance.investor.user,
+            type=notif_type,
+            entity=instance.startup,
+            content=f"{instance.investor.company_name} started following {instance.startup.company_name}"
+        )
+        send_email_notification(
+            instance.investor.user, "new_follower",
+            f"{instance.investor.company_name} started following {instance.startup.company_name}"
+        )
+    except Exception as e:
+        logger.error(f"Error creating new follower notification: {e}")
