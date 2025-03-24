171ab0fb878b6acd02a559c210158497f5282a78
Karina <bogdanova.karina22@gmail.com>
2025-03-24 23:40:08 +0200

    Finished working on Projects

---
+++ projects/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+
+from projects.models import Project
+
+class ProjectListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = (
+            'id',
+            'name',
+            'status',
+            'information',
+            'required_funding',
+            'raised_amount',
+            'company',
+        )
+        
+
+class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = (
+            'id',
+            'name',
+            'status',
+            'information',
+            'required_funding',
+            'raised_amount',
+        )
+        extra_kwargs = {
+            'company': {'required': False}
+        }
+
+    def validate(self, attrs):
+        required_funding = attrs.get("required_funding")
+        raised_amount = attrs.get("raised_amount")
+        
+        if required_funding < 0:
+            raise serializers.ValidationError("Required funding cannot be negative.")
+        if raised_amount < 0:
+            raise serializers.ValidationError("Raised amount cannot be negative.")
+        if raised_amount > required_funding:
+            raise serializers.ValidationError("Raised amount cannot be greater than required funding.")
+        
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        request = self.context.get('request')
+        company_id = request.auth.get('company_id')
+
+        if not company_id:
+            raise serializers.ValidationError("Company ID is missing in the token.")
+
+        validated_data['company_id'] = company_id
+        return super().create(validated_data)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from companies.permissions import IsCompanyMember
 
-# Create your views here.
+from projects.models import Project
+from projects.serializers import ProjectCreateUpdateSerializer, ProjectListSerializer
+
+class ProjectViewSet(ModelViewSet):
+    http_method_names = ('get', 'post', 'put', 'delete')
+    serializer_class = ProjectListSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Project.objects.all()
+    
+    def get_permissions(self):
+        match (self.action):
+            case 'update' | 'partial_update' | 'destroy':
+                return [IsAuthenticated(), IsCompanyMember()]
+            case _:       
+                return super().get_permissions()
+    
+    def get_queryset(self):
+        company_id = self.request.query_params.get("company_id")
+        if company_id:
+            return super().get_queryset().filter(company=company_id)
+
+        return super().get_queryset()
+
+    def get_serializer(self, *args, **kwargs):
+        match (self.action):
+            case 'create' | 'update' | 'partial_update':
+                kwargs['context'] = self.get_serializer_context()
+                return ProjectCreateUpdateSerializer(*args, **kwargs)
+            case _:
+                return super().get_serializer(*args, **kwargs)
