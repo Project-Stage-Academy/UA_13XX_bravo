dae1fc4a94422b296bcb72a14dae059501d62b19
Dmytro Rozhko <d.rozhko.ua@gmail.com>
2025-03-27 20:38:49 +0200

    fixed bug with disconnects

--- communications/consumers.py
+++ communications/consumers.py
@@ -105,7 +105,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         """
         Disconnects the user from the chat group upon exit.
         """
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        if hasattr(self, "room_group_name"):
+            await self.channel_layer.group_discard(
+                self.room_group_name, self.channel_name
+            )
 
     async def receive(self, text_data):
         """
