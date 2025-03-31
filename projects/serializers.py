9af1f937f07d6fa453cb7d2e11aa00929ace5f8a
Dmytro Rozhko <d.rozhko.ua@gmail.com>
2025-03-31 19:54:40 +0300

    added class ProjectStatus addded full explanation status error

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,54 +2,77 @@ from rest_framework import serializers
 
 from projects.models import Project
 
+
 class ProjectListSerializer(serializers.ModelSerializer):
+    status = serializers.ChoiceField(
+        choices=[choice[0] for choice in Project._meta.get_field("status").choices],
+        error_messages={
+            "invalid_choice": "Invalid company status. Choose from: {}".format(
+                ", ".join(
+                    [choice[0] for choice in Project._meta.get_field("status").choices]
+                )
+            )
+        },
+    )
+
     class Meta:
         model = Project
         fields = (
-            'id',
-            'name',
-            'status',
-            'information',
-            'required_funding',
-            'raised_amount',
-            'company',
+            "id",
+            "name",
+            "status",
+            "information",
+            "required_funding",
+            "raised_amount",
+            "company",
         )
-        
+
 
 class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
+    status = serializers.ChoiceField(
+        choices=[choice[0] for choice in Project._meta.get_field("status").choices],
+        error_messages={
+            "invalid_choice": "Invalid company status. Choose from: {}".format(
+                ", ".join(
+                    [choice[0] for choice in Project._meta.get_field("status").choices]
+                )
+            )
+        },
+    )
+
     class Meta:
         model = Project
         fields = (
-            'id',
-            'name',
-            'status',
-            'information',
-            'required_funding',
-            'raised_amount',
+            "id",
+            "name",
+            "status",
+            "information",
+            "required_funding",
+            "raised_amount",
         )
-        extra_kwargs = {
-            'company': {'required': False}
-        }
+        extra_kwargs = {"company": {"required": False}}
 
     def validate(self, attrs):
         required_funding = attrs.get("required_funding")
         raised_amount = attrs.get("raised_amount")
-        
+
         if required_funding < 0:
             raise serializers.ValidationError("Required funding cannot be negative.")
         if raised_amount < 0:
             raise serializers.ValidationError("Raised amount cannot be negative.")
         if raised_amount > required_funding:
-            raise serializers.ValidationError("Raised amount cannot be greater than required funding.")
-        
+            raise serializers.ValidationError(
+                "Raised amount cannot be greater than required funding."
+            )
+
         return super().validate(attrs)
-    
+
     def create(self, validated_data):
-        request = self.context.get('request')
-        company_id = request.auth.get('company_id')
+        request = self.context.get("request")
+        company_id = request.auth.get("company_id")
 
         if not company_id:
             raise serializers.ValidationError("Company ID is missing in the token.")
 
-        validated_data['company_id'] = company_id
+        validated_data["company_id"] = company_id
         return super().create(validated_data)
--- projects/views.py
+++ projects/views.py
@@ -5,19 +5,20 @@ from companies.permissions import IsCompanyMember
 from projects.models import Project
 from projects.serializers import ProjectCreateUpdateSerializer, ProjectListSerializer
 
+
 class ProjectViewSet(ModelViewSet):
-    http_method_names = ('get', 'post', 'put', 'delete')
+    http_method_names = ("get", "post", "put", "delete")
     serializer_class = ProjectListSerializer
     permission_classes = [IsAuthenticated]
     queryset = Project.objects.all()
-    
+
     def get_permissions(self):
         match (self.action):
-            case 'update' | 'partial_update' | 'destroy':
+            case "update" | "partial_update" | "destroy":
                 return [IsAuthenticated(), IsCompanyMember()]
-            case _:       
+            case _:
                 return super().get_permissions()
-    
+
     def get_queryset(self):
         company_id = self.request.query_params.get("company_id")
         if company_id:
@@ -27,8 +28,8 @@ class ProjectViewSet(ModelViewSet):
 
     def get_serializer(self, *args, **kwargs):
         match (self.action):
-            case 'create' | 'update' | 'partial_update':
-                kwargs['context'] = self.get_serializer_context()
+            case "create" | "update" | "partial_update":
+                kwargs["context"] = self.get_serializer_context()
                 return ProjectCreateUpdateSerializer(*args, **kwargs)
             case _:
                 return super().get_serializer(*args, **kwargs)
